16695ffba2a01735131e187bfbe339c88ec2affd
Eugeny Tulupov <Eugeny.Tulupov@noveogroup.com>
2014-08-11 17:14:25 +0700

    Fix email validation

--- common/src/main/java/com/noveogroup/tulupov/guestbook/model/GuestbookEntry.java
+++ common/src/main/java/com/noveogroup/tulupov/guestbook/model/GuestbookEntry.java
@@ -5,6 +5,7 @@ import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Email;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
@@ -36,11 +37,7 @@ public class GuestbookEntry {
     @Setter
     @DatabaseField(columnName = EMAIL, canBeNull = false)
     @Size(max=100, message = "too_large_email")
-    @Pattern(message = "email_wrong",regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"" +
-            "(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])" +
-            "*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0" +
-            "-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\" +
-            "x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
+    @Email
     @NotNull(message = "email_empty")
     private String email;
 
